2a6b900ba55458b860d3eb3ab1b97eec889037ba
Habtamu <habtamugirum0489@gmail.com>
2024-03-20 17:58:10 +0300

    update

--- platform/macOS/productbuild.cmake
+++ platform/macOS/productbuild.cmake
@@ -4,36 +4,6 @@ set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
 set(CPACK_COMMAND_PRODUCTBUILD "${OSQUERY_DATA_PATH}/control/pkg/productbuild.sh")
 set(CPACK_COMMAND_PKGBUILD "${CPACK_COMMAND_PRODUCTBUILD}")
 
-set(CPACK_PRODUCTBUILD_RESOURCES_DIR "${CMAKE_SOURCE_DIR}/Resources")
-# Set the path to the background image file
-set(CPACK_PRODUCTBUILD_BACKGROUND "${CPACK_PRODUCTBUILD_RESOURCES_DIR}/background.png")
-
-# Set the alignment attribute for the background image
-set(CPACK_PRODUCTBUILD_BACKGROUND_ALIGNMENT "center")
-
-# Set the scaling attribute for the background image
-set(CPACK_PRODUCTBUILD_BACKGROUND_SCALING "none")
-
-# Set the MIME type of the background image
-set(CPACK_PRODUCTBUILD_BACKGROUND_MIME_TYPE "image/png")
-
-# Set the UTI type of the background image
-set(CPACK_PRODUCTBUILD_BACKGROUND_UTI "public.png")
-
-# Set the path to the background image file for the Dark Aqua theme
-set(CPACK_PRODUCTBUILD_BACKGROUND_DARKAQUA "${CPACK_PRODUCTBUILD_RESOURCES_DIR}/dark_aqua_background.png")
-
-# Set the alignment attribute for the Dark Aqua background image
-set(CPACK_PRODUCTBUILD_BACKGROUND_DARKAQUA_ALIGNMENT "center")
-
-# Set the scaling attribute for the Dark Aqua background image
-set(CPACK_PRODUCTBUILD_BACKGROUND_DARKAQUA_SCALING "none")
-
-# Set the MIME type of the Dark Aqua background image
-set(CPACK_PRODUCTBUILD_BACKGROUND_DARKAQUA_MIME_TYPE "image/png")
-
-# Set the UTI type of the Dark Aqua background image
-set(CPACK_PRODUCTBUILD_BACKGROUND_DARKAQUA_UTI "public.png")
 
 
 install(
